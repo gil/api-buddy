060047f86f41b95dff6ae8a07adbf107ba75de7d
gil <contato@andregil.org>
2012-02-29 13:31:36 -0300

    more endpoints on sample config.js file

--- src/main/js/Config.holmes.js
+++ src/main/js/Config.holmes.js
@@ -2,7 +2,7 @@ define(function(){
 
 	var Config = {
 
-		url: "http://localhost/holmes/api",
+		url: "http://localhost/holmes",
 
 		methodParam: "_method",
 
@@ -12,7 +12,7 @@ define(function(){
 				name: "Login",
 				endpoints: [
 					{
-						url: "/login",
+						url: "/api/login",
 						method: "POST",
 						description: "Spring Security session-based login Endpoint.",
 						params: [
@@ -31,9 +31,69 @@ define(function(){
 						]
 					},
 					{
-						url: "/keepAlive",
+						url: "/api/logout",
+						method: "GET",
+						description: "Log user out."
+					},
+					{
+						url: "/api/keepAlive",
 						method: "GET",
 						description: "Used to check if the session is still active and returns the logged user data.",
+					},
+					{
+						url: "/api/changePassword",
+						method: "POST",
+						description: "Change logged user's password.",
+						params: [
+							{
+								name: "oldPassword",
+								description: "Actual user password."
+							},
+							{
+								name: "oldPasswordAgain",
+								description: "Actual user password again."
+							},
+							{
+								name: "newPassword",
+								description: "New password. Should be different from the old one and have between 5 and 50 characters."
+							}
+						]
+					},
+					{
+						url: "/public/forgotPassword",
+						method: "POST",
+						description: "Receive a new password via e-mail for given user.",
+						params: [
+							{
+								name: "userEmail",
+								description: "The e-mail address for the user to receive a new password."
+							}
+						]
+					}
+				]
+			},
+
+			{
+				name: "Search",
+				endpoints: [
+					{
+						url: "/api/search",
+						method: "GET",
+						description: "Search for documents stored on Holmes.",
+						params: [
+							{
+								name: "query",
+								description: "The query strin to search for documents."
+							},
+							{
+								name: "start",
+								description: "The first document from result pagination. Starts at 0."
+							},
+							{
+								name: "rows",
+								description: "The amount of documents to receive."
+							}
+						]
 					}
 				]
 			},
@@ -42,29 +102,29 @@ define(function(){
 				name: "Natures",
 				endpoints: [
 					{
-						url: "/natures",
+						url: "/api/natures",
 						method: "GET"
 					},
 					{
-						url: "/natures/{media-id}",
+						url: "/api/natures/{id}",
 						method: "GET",
 						params: [
 							{
-								name: "media-id",
+								name: "id",
 								urlParam: true
 							}
 						]
 					},
 					{
-						url: "/natures",
+						url: "/api/natures",
 						method: "POST"
 					},
 					{
-						url: "/natures",
+						url: "/api/natures",
 						method: "PUT"
 					},
 					{
-						url: "/natures",
+						url: "/api/natures",
 						method: "DELETE"
 					}
 				]
