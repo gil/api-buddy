7fd555da776fa032f12430dfb769b5961289eb66
gil <contato@andregil.org>
2012-03-01 23:54:18 -0300

    updating sample configuration

--- src/main/js/Config.holmes.js
+++ src/main/js/Config.holmes.js
@@ -2,12 +2,15 @@ define(function(){
 
 	var Config = {
 
-		url: "http://localhost/holmes",
+		url: "http://localhost:8080/holmes",
 
 		methodParam: "_method",
 
+		traditionalSerialization: true,
+
 		groups: [
 
+			// Login API
 			{
 				name: "Login",
 				endpoints: [
@@ -73,6 +76,7 @@ define(function(){
 				]
 			},
 
+			// Search API
 			{
 				name: "Search",
 				endpoints: [
@@ -98,37 +102,171 @@ define(function(){
 				]
 			},
 
+			// Natures API
 			{
 				name: "Natures",
 				endpoints: [
 					{
-						url: "/api/natures",
-						method: "GET"
+						url: "/api/natures/simple",
+						method: "GET",
+						description: "List the simplified version of natures and properties for the user."
 					},
 					{
 						url: "/api/natures/{id}",
 						method: "GET",
+						description: "Read the nature from the data base, with the passed ID.",
 						params: [
 							{
 								name: "id",
-								urlParam: true
+								urlParam: true,
+								description: "The nature ID."
 							}
 						]
 					},
 					{
 						url: "/api/natures",
-						method: "POST"
+						method: "GET",
+						description: "List the natures in pages.",
+						params: [
+							{
+								name: "start",
+								description: "The start of the page."
+							},
+							{
+								name: "amount",
+								description: "The amount to be returned."
+							}
+						]
 					},
 					{
 						url: "/api/natures",
-						method: "PUT"
+						method: "POST",
+						description: "Create a new nature with the given attributes.",
+						params: [
+							{
+								name: "name",
+								description: "The nature name."
+							}
+						]
 					},
 					{
 						url: "/api/natures",
-						method: "DELETE"
+						method: "PUT",
+						description: "Actually the update method is not permitted for natures. You can't change the nature's name, and natures only have the name attribute. This method will be used in the future to change other nature's attributes.",
+						params: [
+							{
+								name: "id",
+								description: "The nature ID."
+							},
+							{
+								name: "name",
+								description: "The nature name. (Only here for testing and debugging purpose)"
+							}
+						]
+					},
+					{
+						url: "/api/natures",
+						method: "DELETE",
+						description: "Delete the nature owner of the passed ID.",
+						params: [
+							{
+								name: "id",
+								description: "The nature ID."
+							}
+						]
+					}
+				]
+			},
+			
+			{
+				name: "Properties",
+				endpoints: [
+					{
+						url: "/api/properties/{id}",
+						method: "GET",
+						description: "Read the property from the data base.",
+						params: [
+							{
+								name: "id",
+								urlParam: true,
+								description: "The property ID."
+							}
+						]
+					},
+					{
+						url: "/api/properties",
+						method: "GET",
+						description: "List the properties in pages.",
+						params: [
+							{
+								name: "start",
+								description: "The start of the page."
+							},
+							{
+								name: "amount",
+								description: "The amount to be returned."
+							}
+						]
+					},
+					{
+						url: "/api/properties",
+						method: "POST",
+						description: "Create a new property with the given attributes. The 'values' attribute defines the different values possible for a 'LIST' property. You can add more values to the attribute.",
+						params: [
+							{
+								name: "name",
+								description: "The property name."
+							},
+							{
+								name: "propertyType",
+								description: "The type to describe the property. (Accepted values: 'LIST', 'TEXT', 'DATE', 'BOOLEAN', 'TAG', 'NUMBER')"
+							},
+							{
+								name: "values",
+								description: "One or more values for properties of type LIST.",
+								multiValue: true
+							}
+						]
+					},
+					{
+						url: "/api/properties",
+						method: "PUT",
+						description: "Update the property with the given attributes. The 'values' attribute defines the different values possible for a 'LIST' property. You can add more values to the attribute.",
+						params: [
+							{
+								name: "id",
+								description: "The property ID."
+							},
+							{
+								name: "name",
+								description: "The property name."
+							},
+							{
+								name: "propertyType",
+								description: "The type to describe the property. (Accepted values: 'LIST', 'TEXT', 'DATE', 'BOOLEAN', 'TAG', 'NUMBER')"
+							},
+							{
+								name: "values",
+								description: "One or more values for properties of type LIST.",
+								multiValue: true
+							}
+						]
+					},
+					{
+						url: "/api/properties",
+						method: "DELETE",
+						description: "Delete the property owner of the passed ID.",
+						params: [
+							{
+								name: "id",
+								description: "The property ID."
+							}
+						]
 					}
 				]
 			}
+			
+			// Next Group
 
 		]
 
